e390aafd09e083d3ae42063c785b525bed27c65a
Tobias Norlund <tobias@norlund.se>
2023-08-21 11:19:16 +0200

    ppo on pretrained opt seems to converge also on later master

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
@@ -24,4 +24,5 @@ deepspeed --num_gpus 1 main.py \
    --actor_zero_stage $ACTOR_ZERO_STAGE --critic_zero_stage $CRITIC_ZERO_STAGE \
    --num_padding_at_beginning 1 --gradient_accumulation_steps 2 \
    --deepspeed --actor_lora_dim 128 --enable_hybrid_engine --actor_gradient_checkpointing --disable_actor_dropout \
+   --enable_tensorboard --tensorboard_path $OUTPUT \
    --output_dir $OUTPUT &> $OUTPUT/training.log
